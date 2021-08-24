07156325c15ebde381feff58a454712f0f91b651
Warren W Dixon <warrenwdixon@sbcglobal.net>
2021-08-23 20:49:36 -0500

    wrao xml in layout tag, inflate mainctivity using DataBindingUtil

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,13 +2,18 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import timber.log.Timber
+import com.udacity.shoestore.databinding.ActivityMainBinding
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         Timber.plant(Timber.DebugTree())
     }
 }
