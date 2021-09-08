35dfecbfdbdc6461e7f5e567879f2f920551d06c
Warren Dixon <Warren.Dixon@e-hps.com>
2021-09-08 14:14:31 -0500

    updated LoginFragment to inflate fragment using data binding

--- starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,13 +29,20 @@ class LoginFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
+    private var _binding : FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater, contaq
+        iner: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        //return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+
     }
 
     companion object {
